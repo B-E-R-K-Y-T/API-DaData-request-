f2c0bd0db5327f95b042e9737379aae79c199f6b
BERKYT <BerkyT115@yandex.ru>
2024-08-15 18:50:46 +0300

    added /get_address_view and Dockerfile

--- src/api/v1/geo_dadata.py
+++ src/api/v1/geo_dadata.py
@@ -1,6 +1,7 @@
 from http import HTTPStatus
 
-from fastapi import APIRouter, Depends, Query
+from fastapi import APIRouter, Depends, Query, Request, Response
+from fastapi.templating import Jinja2Templates
 
 from src.schemas.geolocate import Suggestion
 from src.service.client.api_worker import APIWorker
@@ -8,6 +9,10 @@ from src.service.client.api_worker import APIWorker
 router = APIRouter(
     prefix="/geolocate",
 )
+templates = Jinja2Templates(directory="src/templates")
+
+_RADIUS_METERS_CONSTRAIN = Query(100, ge=1, le=1000)
+_COUNT_CONSTRAIN = Query(10, ge=1, le=100)
 
 
 @router.get(
@@ -19,8 +24,39 @@ router = APIRouter(
 async def get_address(
     lat: float,
     lon: float,
-    radius_meters: int = Query(100, ge=1, le=1000),
-    count: int = Query(10, ge=1, le=100),
+    radius_meters: int = _RADIUS_METERS_CONSTRAIN,
+    count: int = _COUNT_CONSTRAIN,
     api_worker: APIWorker = Depends(APIWorker),
 ) -> list[dict]:
     return await api_worker.get_address(lat, lon, radius_meters, count)
+
+
+@router.get(
+    "/address_view",
+    description="Находит ближайшие адреса (дома, улицы, города) по географическим координатам. "
+    "И возвращает HTML. Только для России.",
+    response_model=list[Suggestion],
+    status_code=HTTPStatus.OK,
+)
+async def get_address_view(
+    request: Request,
+    lat: float,
+    lon: float,
+    radius_meters: int = _RADIUS_METERS_CONSTRAIN,
+    count: int = _COUNT_CONSTRAIN,
+    api_worker: APIWorker = Depends(APIWorker),
+) -> Response:
+    data = await api_worker.get_address(lat, lon, radius_meters, count)
+    addresses = []
+
+    for item in data:
+        addresses.append(
+            {
+                "value": item.get("value"),
+                "unrestricted_value": str(item.get("unrestricted_value")).split(","),
+            }
+        )
+
+    return templates.TemplateResponse(
+        request=request, name="address.html", context={"addresses": addresses}
+    )
--- tests/api/unit_tests/test_geolocate.py
+++ tests/api/unit_tests/test_geolocate.py
@@ -2,18 +2,20 @@ import http
 
 import pytest
 
+MOSCOW_CORDS = (55.878, 37.653)
+
 
 @pytest.mark.parametrize(
     ["lat", "lon", "radius_meters", "count"],
     [
-        (45.232, 122.41, 1, 1),
+        (*MOSCOW_CORDS, 1, 1),
         (45.232, -122.41, 1000, 100),
         (-45.232, 122.411, 10, 10),
         (-45.232, -122.41, 10, 10),
-        (55.878, 37.653, 10, 10),
+        (*MOSCOW_CORDS, 10, 10),
     ],
 )
-def test_geolocate(
+def test_geolocate_address(
     lat: float, lon: float, radius_meters: int, count: int, base_url, client
 ):
     args = (
@@ -22,7 +24,15 @@ def test_geolocate(
     )
 
     assert (
-        client(*args, params={"lat": lat, "lon": lon}).status_code == http.HTTPStatus.OK
+        client(
+            *args,
+            params={
+                "lat": lat,
+                "lon": lon,
+                "radius_meters": radius_meters,
+                "count": count,
+            },
+        ).status_code == http.HTTPStatus.OK
     )
 
 
@@ -37,7 +47,7 @@ def test_geolocate(
         (0.0, 0.0, 1001, 1, http.HTTPStatus.UNPROCESSABLE_ENTITY),
     ],
 )
-def test_geolocate_fail(
+def test_geolocate_address_fail(
     lat: float,
     lon: float,
     radius_meters: int,
@@ -60,6 +70,70 @@ def test_geolocate_fail(
                 "radius_meters": radius_meters,
                 "count": count,
             },
-        ).status_code
-        == status_code
+        ).status_code == status_code
+    )
+
+
+@pytest.mark.parametrize(
+    ["lat", "lon", "radius_meters", "count"],
+    [
+        (*MOSCOW_CORDS, 1, 1),
+    ],
+)
+def test_geolocate_address_view(
+    lat: float, lon: float, radius_meters: int, count: int, base_url, client
+):
+    args = (
+        "GET",
+        f"{base_url}/geolocate/address_view",
+    )
+
+    assert (
+        client(
+            *args,
+            params={
+                "lat": lat,
+                "lon": lon,
+                "radius_meters": radius_meters,
+                "count": count,
+            },
+        ).status_code == http.HTTPStatus.OK
+    )
+
+
+@pytest.mark.parametrize(
+    ["lat", "lon", "radius_meters", "count", "status_code"],
+    [
+        (0.0, 2342340.0243234, 1, 1, http.HTTPStatus.BAD_REQUEST),
+        (111111.0, 0.0, 1, 1, http.HTTPStatus.BAD_REQUEST),
+        (0.0, 0.0, -1, 1, http.HTTPStatus.UNPROCESSABLE_ENTITY),
+        (0.0, 0.0, 1, -1, http.HTTPStatus.UNPROCESSABLE_ENTITY),
+        (0.0, 0.0, 1, 101, http.HTTPStatus.UNPROCESSABLE_ENTITY),
+        (0.0, 0.0, 1001, 1, http.HTTPStatus.UNPROCESSABLE_ENTITY),
+    ],
+)
+def test_geolocate_address_view_fail(
+    lat: float,
+    lon: float,
+    radius_meters: int,
+    count: int,
+    status_code: int,
+    base_url,
+    client,
+):
+    args = (
+        "GET",
+        f"{base_url}/geolocate/address_view",
+    )
+
+    assert (
+        client(
+            *args,
+            params={
+                "lat": lat,
+                "lon": lon,
+                "radius_meters": radius_meters,
+                "count": count,
+            },
+        ).status_code == status_code
     )
