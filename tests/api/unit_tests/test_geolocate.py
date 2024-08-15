9748d8b5921389adaa997af58214c627457538c2
BERKYT <BerkyT115@yandex.ru>
2024-08-15 19:22:39 +0300

    refactor

--- tests/api/unit_tests/test_geolocate.py
+++ tests/api/unit_tests/test_geolocate.py
@@ -1,10 +1,10 @@
 import http
-
 import pytest
 
 _MOSCOW_CORDS = (55.878, 37.653)
+
+# Сценарии ошибок
 _ERROR_SCENARIOS_PARAMS = [
-    # "lat", "lon", "radius_meters", "count"
     (0.0, 2342340.0243234, 1, 1, http.HTTPStatus.BAD_REQUEST),
     (111111.0, 0.0, 1, 1, http.HTTPStatus.BAD_REQUEST),
     (0.0, 0.0, -1, 1, http.HTTPStatus.UNPROCESSABLE_ENTITY),
@@ -12,6 +12,21 @@ _ERROR_SCENARIOS_PARAMS = [
     (0.0, 0.0, 1, 101, http.HTTPStatus.UNPROCESSABLE_ENTITY),
     (0.0, 0.0, 1001, 1, http.HTTPStatus.UNPROCESSABLE_ENTITY),
 ]
+
+
+# Общая функция для выполнения запроса
+def perform_get_request(client, base_url, endpoint, lat, lon, radius_meters, count):
+    response = client(
+        "GET",
+        f"{base_url}/{endpoint}",
+        params={
+            "lat": lat,
+            "lon": lon,
+            "radius_meters": radius_meters,
+            "count": count,
+        },
+    )
+    return response.status_code
 
 
 @pytest.mark.parametrize(
@@ -24,25 +39,11 @@ _ERROR_SCENARIOS_PARAMS = [
         (*_MOSCOW_CORDS, 10, 10),
     ],
 )
-def test_geolocate_address(
-    lat: float, lon: float, radius_meters: int, count: int, base_url, client
-):
-    args = (
-        "GET",
-        f"{base_url}/geolocate/addresses",
+def test_geolocate_address(lat, lon, radius_meters, count, base_url, client):
+    resp_status_code = perform_get_request(
+        client, base_url, "geolocate/addresses", lat, lon, radius_meters, count
     )
-
-    assert (
-        client(
-            *args,
-            params={
-                "lat": lat,
-                "lon": lon,
-                "radius_meters": radius_meters,
-                "count": count,
-            },
-        ).status_code == http.HTTPStatus.OK
-    )
+    assert resp_status_code == http.HTTPStatus.OK
 
 
 @pytest.mark.parametrize(
@@ -50,57 +51,24 @@ def test_geolocate_address(
     _ERROR_SCENARIOS_PARAMS,
 )
 def test_geolocate_address_fail(
-    lat: float,
-    lon: float,
-    radius_meters: int,
-    count: int,
-    status_code: int,
-    base_url,
-    client,
+    lat, lon, radius_meters, count, status_code, base_url, client
 ):
-    args = (
-        "GET",
-        f"{base_url}/geolocate/addresses",
+    resp_status_code = perform_get_request(
+        client, base_url, "geolocate/addresses", lat, lon, radius_meters, count
     )
-
-    assert (
-        client(
-            *args,
-            params={
-                "lat": lat,
-                "lon": lon,
-                "radius_meters": radius_meters,
-                "count": count,
-            },
-        ).status_code == status_code
-    )
+    assert resp_status_code == status_code
 
 
 @pytest.mark.parametrize(
     ["lat", "lon", "radius_meters", "count"],
-    [
-        (*_MOSCOW_CORDS, 1, 1),
-    ],
+    [(*_MOSCOW_CORDS, 1, 1)],
 )
-def test_geolocate_address_view(
-    lat: float, lon: float, radius_meters: int, count: int, base_url, client
-):
-    args = (
-        "GET",
-        f"{base_url}/geolocate/addresses_view",
+def test_geolocate_address_view(lat, lon, radius_meters, count, base_url, client):
+    resp_status_code = perform_get_request(
+        client, base_url, "geolocate/addresses_view", lat, lon, radius_meters, count
     )
 
-    assert (
-        client(
-            *args,
-            params={
-                "lat": lat,
-                "lon": lon,
-                "radius_meters": radius_meters,
-                "count": count,
-            },
-        ).status_code == http.HTTPStatus.OK
-    )
+    assert resp_status_code == http.HTTPStatus.OK
 
 
 @pytest.mark.parametrize(
@@ -108,27 +76,9 @@ def test_geolocate_address_view(
     _ERROR_SCENARIOS_PARAMS,
 )
 def test_geolocate_address_view_fail(
-    lat: float,
-    lon: float,
-    radius_meters: int,
-    count: int,
-    status_code: int,
-    base_url,
-    client,
+    lat, lon, radius_meters, count, status_code, base_url, client
 ):
-    args = (
-        "GET",
-        f"{base_url}/geolocate/addresses_view",
+    resp_status_code = perform_get_request(
+        client, base_url, "geolocate/addresses_view", lat, lon, radius_meters, count
     )
-
-    assert (
-        client(
-            *args,
-            params={
-                "lat": lat,
-                "lon": lon,
-                "radius_meters": radius_meters,
-                "count": count,
-            },
-        ).status_code == status_code
-    )
+    assert resp_status_code == status_code
